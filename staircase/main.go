d211b32bde595a53b83ba8a98bab1c3ae19a1477
paulo <paulo@brandwatch.com>
2015-11-06 13:48:36 +0000

    staircase

---
+++ staircase/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+
+func Staircase (num int) {
+
+    n := num
+    for n > 0 {
+        for i := 0; i < num; i++ {
+            if i < n - 1 {
+                fmt.Print(" ")
+            } else {
+                fmt.Print("#")
+            }
+        }
+        fmt.Println()
+        n = n - 1
+    }
+}
+
+
+func main() {
+    var num int
+    fmt.Scanf("%d", &num)
+    Staircase(num)
+}
